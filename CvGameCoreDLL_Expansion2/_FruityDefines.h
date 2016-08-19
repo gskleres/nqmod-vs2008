11e4d7c3b43d78ab75e5ff92f623f555d60160e1
gskleres <george.skleres@gmail.com>
2016-08-18 18:12:23 -0700

    Fruity Defines
    Adding Fruity Defines, disabled

---
+++ CvGameCoreDLL_Expansion2/_FruityDefines.h
@@ -0,0 +1,16 @@
+// FruityDefines.h
+#pragma once
+
+//#ifndef FRUITY_DEFINES_LIST	// uncomment to use Fruity defines
+#ifdef FRUITY_DEFINES_LIST		// comment to use Fruity defines
+
+// List of defines that are supposed to be for new additions and can be easily toggled on and off (plus they help with merging)
+#define FRUITY_DEFINES_LIST
+
+
+// - TRADITION -
+//#define FRUITY_TRADITION_ARISTOCRACY
+#define FRUITY_TRADITION_LANDED_ELITE
+
+
+#endif
