4fcd06d26290c8738be0b3e48b8111d7e0706358
gskleres <george.skleres@gmail.com>
2017-03-29 20:07:30 -0400

    cleaned up covert action
    just a few things

--- NQMP/ideologies/freedom/T1_5CovertAction.lua
+++ NQMP/ideologies/freedom/T1_5CovertAction.lua
@@ -2,8 +2,8 @@ function NQMP_T1_Covert_Action_OnPolicyAdopted(playerID, policyID)
 	local player = Players[playerID]
 	if (policyID == GameInfo.Policies["POLICY_COVERT_ACTION"].ID) then
 		local pCapital = player:GetCapitalCity()
-		pCapital:SetNumRealBuilding(GameInfoTypes["BUILDING_T1_COVERT_ACTION"], 1)
-		pCapital:SetNumRealBuilding(GameInfoTypes["BUILDING_T1_COVERT_ACTION"], 0)
+		pCapital:SetNumRealBuilding(GameInfoTypes["DUMMY_BUILDING_T1_COVERT_ACTION"], 1)
+		pCapital:SetNumRealBuilding(GameInfoTypes["DUMMY_BUILDING_T1_COVERT_ACTION"], 0)
 	end
 end
 GameEvents.PlayerAdoptPolicy.Add(NQMP_T1_Covert_Action_OnPolicyAdopted)
